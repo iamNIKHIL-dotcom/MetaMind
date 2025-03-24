f9a5e3d2600c541b5380e672913253df5d50df09
iamNIKHIL-dotcom <ummidinikhil@gmail.com>
2025-03-24 23:09:53 +0530

    validations

---
+++ src/validations.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+export const signupSchema = z.object({
+
+})
+
+export const signinSchema = z.object({
+
+})
+
+export const shareSchema = z.object({
+
+})
+
+export const contentSchema = z.object({
+
+})
+
